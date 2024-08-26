f02aab4333ba42ccd5a127b4c3440775572f6a3e
OldResolution <140333635+OldResolution@users.noreply.github.com>
2024-08-26 19:38:59 +0530

    Revert "Satellite Layer"
    This reverts commit aaec3ccd8b25c846b084ae33d852bc9a6305e04f.

--- script.js
+++ script.js
@@ -52,11 +52,9 @@ var deLayer = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
 });
 
 // Satellite layer
-var satelliteLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
+var satelliteLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
 });
 
 // Dark Layer 
